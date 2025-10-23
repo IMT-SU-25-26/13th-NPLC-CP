cd24e9d002bf03982db46be1d415b147d772fdb0
Verzional <verzional777@gmail.com>
2025-10-23 22:16:21 +0700

    App Finalized

--- src/components/pages/waiting/Waiting.tsx
+++ src/components/pages/waiting/Waiting.tsx
@@ -9,6 +9,7 @@ export default function Waiting({
 }: WaitingProps) {
   const isPending = status === ContestStatus.PENDING;
   const isPaused = status === ContestStatus.PAUSED;
+  const isRunning = status === ContestStatus.RUNNING;
 
   return (
     <div className="relative z-[100] w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] backdrop-blur-2xl flex flex-col items-center justify-center gap-6 p-8 lg:p-12 rounded-xl shadow-lg border-8 border-[#FCF551]">
@@ -50,7 +51,13 @@ export default function Waiting({
 
         {/* Title */}
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-[#75E8F0] [text-shadow:_0_0_30px_rgba(0,255,255,1)] mb-4">
-          {isPending ? "Contest Has Not Started Yet" : isPaused ? "Contest Is Paused" : "Contest Has Ended"}
+          {isPending
+            ? "Contest Has Not Started Yet"
+            : isPaused
+            ? "Contest Is Paused"
+            : isRunning
+            ? "Contest Is Running"
+            : "Contest Has Ended"}
         </h1>
 
         {/* Description */}
@@ -59,6 +66,8 @@ export default function Waiting({
             ? "The competition hasn't begun yet. Please wait for the admin to start the contest."
             : isPaused
             ? "The competition is currently paused. Please wait for the admin to resume the contest."
+            : isRunning
+            ? "The competition is currently running. Please navigate to the problems page to continue, good luck!"
             : "The competition has concluded. Thank you for participating!"}
         </p>
       </div>
