42e670f0210cff22b5e2eabe381f24114d634eca
Verzional <verzional777@gmail.com>
2025-11-07 01:24:19 +0700

    Pusher Fix

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -22,3 +22,19 @@ export const pusherServer = new PusherServer({
   port: process.env.SOKETI_PORT!,
   useTLS: process.env.SOKETI_TLS === "true",
 });
+
+// export const pusherClient = new PusherClient(
+//   process.env.NEXT_PUBLIC_PUSHER_KEY!,
+//   {
+//     cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+//     forceTLS: true,
+//   }
+// );
+
+// export const pusherServer = new PusherServer({
+//   appId: process.env.PUSHER_APP_ID!,
+//   key: process.env.PUSHER_KEY!,
+//   secret: process.env.PUSHER_SECRET!,
+//   cluster: process.env.PUSHER_CLUSTER!,
+//   useTLS: true,
+// });
